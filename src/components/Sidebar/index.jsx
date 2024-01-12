4219f34727099e207484d6a2d06cdefd38b97430
Jasurbek <jasurdev1604@gmail.com>
2024-01-12 10:45:35 +0500

    set title to page

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,7 +3,13 @@ import { Container, Title, Link } from "./style";
 import { routes } from "../../utils/routes";
 
 const Sidebar = ({ mode }) => {
-  const [active, setActive] = useState("/");
+  const [path, setPath] = useState("");
+  function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+  document.title = `Ant Design ${
+    path !== "/" ? ` - ${capitalizeFirstLetter(path.slice(1))}` : ""
+  }`;
   return (
     <Container mode={mode}>
       {routes.map(({ id, title, link, path }) =>
@@ -12,12 +18,7 @@ const Sidebar = ({ mode }) => {
             {title}
           </Title>
         ) : (
-          <Link
-            key={id}
-            active={`${active === window.location.pathname}`}
-            to={path}
-            mode={mode}
-          >
+          <Link key={id} to={path} onClick={() => setPath(path)} mode={mode}>
             {link}
           </Link>
         )
